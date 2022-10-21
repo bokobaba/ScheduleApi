43ec37681301da07f0efe49e276a80c73ba48223
Love <Love@DESKTOP-CR6LECC>
2022-10-21 14:11:32 -0500

    program swagger updated

--- ScheduleApi/Program.cs
+++ ScheduleApi/Program.cs
@@ -121,10 +121,10 @@ builder.Services.AddDbContext<ScheduleDbContext>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment()) {
-app.UseSwagger();
-app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors(myAllowSpecificOrigins);
 
